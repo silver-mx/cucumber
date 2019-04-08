38f2b0e6c0e45b69db2f39669994d97bd645fff4
Diego Núñez Silva <diego.silva@cepheid.com>
2019-04-08 21:17:32 +0200

    Update code to generate report

--- src/test/java/org/selenide/examples/cucumber/GoogleTest.java
+++ src/test/java/org/selenide/examples/cucumber/GoogleTest.java
@@ -2,6 +2,8 @@ package org.selenide.examples.cucumber;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.junit.TextReport;
+
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.Before;
 import org.junit.Rule;
@@ -9,6 +11,13 @@ import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources",
+    glue= {"org.selenide.examples.cucumber"},
+    plugin = { "pretty", "html:target/cucumber-reports", "json:target/cucumber-reports/cucumber.json"  },
+    strict = true,
+    monochrome = true
+    )
 public class GoogleTest {
   @Rule
   public TestRule report = new TextReport().onFailedTest(true).onSucceededTest(true);
--- src/test/resources/org/selenide/examples/cucumber/google-text-search.feature
+++ src/test/resources/org/selenide/examples/cucumber/google-text-search.feature
@@ -13,3 +13,9 @@ Feature: Google text search
     When a keyword "селенид тесты" is entered in input field
     Then at least top 5 matches should be shown
     Then the first one should contain selenide.org
+    
+
+  Scenario: user can search any keyword in Spanish
+
+    Given an open browser with google.com
+    When this is  step not yet implemented
